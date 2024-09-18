5c703fe231fac3f70af11791858d4b4fbe75d664
trevinlarsen <soccerbeasttl23@gmail.com>
2024-09-18 13:14:47 -0600

    day 3

---
+++ sandbox/wk1/1prac.js
@@ -0,0 +1,8 @@
+console.log ("hello");
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
